ffd8704faece379c3d98e065ebdb4d199b905fc4
Basant <59922781+Basant-innovation@users.noreply.github.com>
2020-04-09 18:47:04 +0300

    ObserverDesignPattern

---
+++ ObserverDesignPattern/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SocialMediaNotification SMNotification = new SocialMediaNotification();
+            SocialMediaUsers user1 = new SocialMediaUsers();
+            SocialMediaUsers user2 = new SocialMediaUsers();
+
+            // Adding two users to the recieve notification
+            SMNotification.Subscribe(user1);
+            SMNotification.Subscribe(user2);
+
+            // Notifying users (observers)
+            SMNotification.Notify();
+
+            // A user has decided not to recieve notifications
+            SMNotification.Unsubscribe(user1);
+
+            // Notifying the updated list of users
+           SMNotification.Notify();
+            Console.Read();
+        }
+    }
+}
---
+++ ObserverDesignPattern/SocialMediaNotification.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverDesignPattern
+{
+    class SocialMediaNotification: ISubject
+    {
+
+        public int State { get; set; } = -0;
+
+        private List<IObserver> _observers = new List<IObserver>();
+
+        public void Subscribe(IObserver observer)
+        {
+            Console.WriteLine("A user subscribe to receive notifications");
+            this._observers.Add(observer);
+        }
+
+        public void Unsubscribe(IObserver observer)
+        {
+            this._observers.Remove(observer);
+            Console.WriteLine("A user unsubscribe to receive notifications");
+        }
+
+        // Trigger an update in each subscriber.
+        public void Notify()
+        {
+            Console.WriteLine("Notifications:");
+            foreach (var observer in _observers)
+            {
+                observer.update();
+            }
+        }
+
+    }
+}
