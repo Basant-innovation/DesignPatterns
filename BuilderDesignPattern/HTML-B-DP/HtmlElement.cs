6ea9d3cd5bef940d787ab98b142591e7201143b7
Basant <59922781+Basant-innovation@users.noreply.github.com>
2020-04-09 18:34:22 +0300

    Add files via upload

---
+++ BuilderDesignPattern/HTML-B-DP/HtmlElement.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderDesignPatternHTML
+{
+    class HtmlElement
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+        private int indentSize = 2;
+        public List<HtmlElement> Elements = new List<HtmlElement>();
+
+        public HtmlElement()
+        {
+
+        }
+        public HtmlElement(string name, string text)
+        {
+            Name = name;
+            Text = text;
+        }
+
+        private string ToStringBuilder(int indent)
+        {
+            var sb = new StringBuilder();
+            var i = new string(' ', indentSize * indent);
+            sb.Append($"{i}<{Name}>\n");
+            if (!string.IsNullOrWhiteSpace(Text))
+            {
+                sb.Append(new string(' ', indentSize * (indent + 1)));
+                sb.Append(Text);
+                sb.Append("\n");
+            }
+
+            foreach (var e in Elements)
+                sb.Append(e.ToStringBuilder(indent + 1));
+
+            sb.Append($"{i}</{Name}>\n");
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToStringBuilder(0);
+        }
+    }
+}
---
+++ BuilderDesignPattern/HTML-B-DP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderDesignPatternHTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var htmlBuilder = new HtmlBuilder("ul");
+            htmlBuilder.AddChildFluent("li", "first")
+                .AddChildFluent("li", "second")
+                .AddChildFluent("li", "third");
+            Console.WriteLine(htmlBuilder);
+            htmlBuilder.Clear();
+            Console.WriteLine(htmlBuilder);
+            Console.Read();
+        }
+    }
+}
