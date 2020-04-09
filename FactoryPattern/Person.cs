8b970866ed30dd07718ca011267bf7bcb184f024
Basant <59922781+Basant-innovation@users.noreply.github.com>
2020-04-09 18:42:13 +0300

    FactoryPattern

---
+++ FactoryPattern/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    class Person
+    {
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual DateTime StartDate { get; set; }
+        public virtual int Rating { get; set; }
+    }
+}
